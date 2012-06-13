fb42923f9500a4711444457e1ec4151976694d6a
fancyoung <siyang1982@msn.com>
2012-06-13 10:19:00 +0800

    add uniq-line

--- init.el
+++ init.el
@@ -379,5 +379,26 @@
 ;;hs-mirror-mode模式下用f9切换折叠
 (global-set-key [C-f1] 'hs-toggle-hiding)
 
+;;删除重复行 M-x uniq-line
+(defun uniq-lines (beg end)
+  "Unique lines in region.
+Called from a program, there are two arguments:
+BEG and END (region to sort)."
+  (interactive "r")
+  (save-excursion
+    (save-restriction
+      (narrow-to-region beg end)
+      (goto-char (point-min))
+      (while (not (eobp))
+        (kill-line 1)
+        (yank)
+        (let ((next-line (point)))
+          (while
+              (re-search-forward
+               (format "^%s" (regexp-quote (car kill-ring))) nil t)
+            (replace-match "" nil nil))
+          (goto-char next-line))))))
+
+
 ;;;;;;------====== self ======------;;;;;;
 ;;;(require 'private)
